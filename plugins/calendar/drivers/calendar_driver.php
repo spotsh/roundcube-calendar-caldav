a13fec7a21ad9f93029ee448bff88143440b7964
Aleksander Machniak (Kolab Systems) <machniak@kolabsys.com>
2011-07-27 11:20:27 +0200

    Fixed default calendar edit form handling

--- plugins/calendar/drivers/calendar_driver.php
+++ plugins/calendar/drivers/calendar_driver.php
@@ -305,10 +305,11 @@ abstract class calendar_driver
    *
    * @param string Request action 'form-edit|form-new'
    * @param array  Calendar properties (e.g. id, color)
+   * @param string HTML code of default edit form
    *
    * @return string HTML content of the form
    */
-  public function calendar_form($action, $calendar)
+  public function calendar_form($action, $calendar, $html)
   {
     return null;
   }
