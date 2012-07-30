ca3134b312f9ee19484f377a2413907b146697d5
Aleksander Machniak <alec@alec.pl>
2012-07-30 15:05:08 +0200

    ActiveSync configuration plugin (based on kolab_zpush)

---
+++ plugins/kolab_activesync/kolab_activesync.js
@@ -0,0 +1,182 @@
+/**
+ * Client scripts for the Kolab ActiveSync configuration utitlity
+ *
+ * @version @package_version@
+ * @author Aleksander Machniak <machniak@kolabsys.com>
+ * @author Thomas Bruederli <bruederli@kolabsys.com>
+ *
+ * Copyright (C) 2011-2012, Kolab Systems AG <contact@kolabsys.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+function kolab_activesync_config()
+{
+    /* private members */
+    var me = this;
+    var http_lock = null;
+    var active_device = null;
+
+
+    /* constructor */
+    var devicelist = new rcube_list_widget(rcmail.gui_objects.devicelist,
+        { multiselect:true, draggable:false, keyboard:true });
+    devicelist.addEventListener('select', select_device);
+    devicelist.init();
+
+    rcmail.register_command('plugin.save-config', save_config);
+    rcmail.register_command('plugin.delete-device', delete_device_config);
+    rcmail.addEventListener('plugin.activesync_data_ready', device_data_ready);
+    rcmail.addEventListener('plugin.activesync_save_complete', save_complete);
+
+    $('input.alarm').change(function(e){ if (this.checked) $('#'+this.id.replace(/_alarm/, '')).prop('checked', this.checked); });
+    $('input.subscription').change(function(e){ if (!this.checked) $('#'+this.id+'_alarm').prop('checked', false); });
+    $(window).bind('resize', resize_ui);
+
+    $('.subscriptionblock thead td.subscription img, .subscriptionblock thead td.alarm img').click(function(e){
+      var $this = $(this);
+      var classname = $this.parent().get(0).className;
+      var check = !($this.data('checked') || false);
+      $this.css('cursor', 'pointer').data('checked', check)
+        .closest('table').find('input.'+classname).prop('checked', check).change();
+    });
+
+    // select the one and only device from list
+    if (rcmail.env.devicecount == 1) {
+        for (var imei in rcmail.env.devices)
+            break;
+        devicelist.select(imei);
+    }
+
+    /* private methods */
+    function select_device(list)
+    {
+        active_device = list.get_single_selection();
+        rcmail.enable_command('plugin.save-config', 'plugin.delete-device', true);
+
+        if (active_device) {
+            http_lock = rcmail.set_busy(true, 'loading');
+            rcmail.http_request('plugin.activesyncjson', { cmd:'load', id:active_device }, http_lock);
+        }
+
+        $('#introtext').hide();
+    }
+
+    // callback from server after loading device data
+    function device_data_ready(data)
+    {
+        // reset form first
+        $('input.alarm:checked').prop('checked', false);
+        $('input.subscription:checked').prop('checked', false).change();
+
+        if (data.id && data.id == active_device) {
+            $('#config-device-alias').val(data.devicealias);
+//            $('#config-device-mode').val(data.syncmode);
+//            $('#config-device-laxpic').prop('checked', data.laxpic ? true : false);
+
+            $('input.subscription').each(function(i, elem){
+                var key = elem.value;
+                elem.checked = data.subscribed[key] ? true : false;
+            }).change();
+            $('input.alarm').each(function(i, elem){
+                var key = elem.value;
+                elem.checked = data.subscribed[key] == 2;
+            });
+
+            $('#configform, #prefs-box .boxtitle').show();
+            resize_ui();
+        }
+        else {
+            $('#configform, #prefs-box .boxtitle').hide();
+        }
+    }
+
+    // submit current configuration form to server
+    function save_config()
+    {
+        // TODO: validate device info
+        var data = {
+            cmd: 'save',
+            id: active_device,
+            devicealias: $('#config-device-alias').val(),
+//            syncmode: $('#config-device-mode option:selected').val(),
+//            laxpic: $('#config-device-laxpic').get(0).checked ? 1 : 0
+        };
+
+        data.subscribed = {};
+        $('input.subscription:checked').each(function(i, elem){
+            data.subscribed[elem.value] = 1;
+        });
+        $('input.alarm:checked').each(function(i, elem){
+            if (data.subscribed[elem.value])
+                data.subscribed[elem.value] = 2;
+        });
+
+        http_lock = rcmail.set_busy(true, 'kolab_activesync.savingdata');
+        rcmail.http_post('plugin.activesyncjson', data, http_lock);
+    }
+
+    // callback function when saving has completed
+    function save_complete(p)
+    {
+        if (p.success && p.devicealias) {
+            $('#devices-table tr.selected span.devicealias').html(p.devicealias);
+            rcmail.env.devices[p.id].ALIAS = p.devicealias;
+        }
+    }
+
+    // handler for delete commands
+    function delete_device_config()
+    {
+        if (active_device && confirm(rcmail.gettext('devicedeleteconfirm', 'kolab_activesync'))) {
+            http_lock = rcmail.set_busy(true, 'kolab_activesync.savingdata');
+            rcmail.http_post('plugin.activesyncjson', { cmd:'delete', id:active_device }, http_lock);
+        }
+    }
+
+    // handler for window resize events: sets max-height of folders list scroll container
+    function resize_ui()
+    {
+        if (active_device) {
+//@TODO: this doesn't work good with Larry
+            var h = $(window).height(), pos = $('#foldersubscriptions').offset();
+            $('#foldersubscriptions').css('max-height', (h - pos.top - 90) + 'px');
+        }
+    }
+}
+
+
+window.rcmail && rcmail.addEventListener('init', function(evt) {
+    var ACTION_CONFIG = 'plugin.activesyncconfig';
+
+    // add button to tabs list
+    var tab = $('<span>').attr('id', 'settingstabpluginactivesyncconfig').addClass('tablink');
+    var button = $('<a>').attr('href', rcmail.env.comm_path+'&_action=plugin.activesyncconfig').html(rcmail.gettext('tabtitle', 'kolab_activesync')).appendTo(tab);
+    rcmail.add_element(tab, 'tabs');
+
+    if (rcmail.env.action == ACTION_CONFIG)
+        new kolab_activesync_config();
+});
+
+
+// extend jQuery
+(function($){
+  $.fn.serializeJSON = function(){
+    var json = {};
+    jQuery.map($(this).serializeArray(), function(n, i) {
+      json[n['name']] = n['value'];
+    });
+    return json;
+  };
+})(jQuery);
---
+++ plugins/kolab_activesync/kolab_activesync_ui.php
@@ -0,0 +1,183 @@
+<?php
+
+/**
+ * Z-Push configuration user interface builder
+ *
+ * @version @package_version@
+ * @author Thomas Bruederli <bruederli@kolabsys.com>
+ * @author Aleksander Machniak <machniak@kolabsys.com>
+ *
+ * Copyright (C) 2011-2012, Kolab Systems AG <contact@kolabsys.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+class kolab_activesync_ui
+{
+    private $rc;
+    private $plugin;
+
+    public function __construct($plugin)
+    {
+        $this->plugin    = $plugin;
+        $this->rc        = rcmail::get_instance();
+        $skin_path       = $this->plugin->local_skin_path() . '/';
+        $this->skin_path = 'plugins/kolab_activesync/' . $skin_path;
+
+        $this->plugin->include_stylesheet($skin_path . 'config.css');
+        $this->rc->output->include_script('list.js');
+    }
+
+
+    public function device_list($attrib = array())
+    {
+        $attrib += array('id' => 'devices-list');
+
+        $devices = $this->plugin->list_devices();
+        $table = new html_table();
+
+        foreach ($devices as $id => $device) {
+            $name = $device['ALIAS'] ? $device['ALIAS'] : $id;
+            $table->add_row(array('id' => 'rcmrow' . $id));
+            $table->add(null, html::span('devicealias', Q($name)) . html::span('devicetype', Q($device['TYPE'])));
+        }
+
+        $this->rc->output->add_gui_object('devicelist', $attrib['id']);
+        $this->rc->output->set_env('devices', $devices);
+        $this->rc->output->set_env('devicecount', count($devices));
+
+        return $table->show($attrib);
+    }
+
+
+    public function device_config_form($attrib = array())
+    {
+        $table = new html_table(array('cols' => 2));
+
+        $field_id = 'config-device-alias';
+        $input = new html_inputfield(array('name' => 'devicealias', 'id' => $field_id, 'size' => 40));
+        $table->add('title', html::label($field_id, $this->plugin->gettext('devicealias')));
+        $table->add(null, $input->show());
+/*
+        $field_id = 'config-device-mode';
+        $select = new html_select(array('name' => 'syncmode', 'id' => $field_id));
+        $select->add(array($this->plugin->gettext('modeauto'), $this->plugin->gettext('modeflat'), $this->plugin->gettext('modefolder')), array('-1', '0', '1'));
+        $table->add('title', html::label($field_id, $this->plugin->gettext('syncmode')));
+        $table->add(null, $select->show('-1'));
+
+        $field_id = 'config-device-laxpic';
+        $checkbox = new html_checkbox(array('name' => 'laxpic', 'value' => '1', 'id' => $field_id));
+        $table->add('title', $this->plugin->gettext('imageformat'));
+        $table->add(null, html::label($field_id, $checkbox->show() . ' ' . $this->plugin->gettext('laxpiclabel')));
+*/
+        if ($attrib['form'])
+            $this->rc->output->add_gui_object('editform', $attrib['form']);
+
+        return $table->show($attrib);
+    }
+
+
+    public function folder_subscriptions($attrib = array())
+    {
+        if (!$attrib['id'])
+            $attrib['id'] = 'foldersubscriptions';
+
+        // group folders by type (show only known types)
+        $folder_groups = array('mail' => array(), 'contact' => array(), 'event' => array(), 'task' => array());
+        $folder_meta   = $this->plugin->folder_meta();
+        $folder_types  = $this->plugin->list_types();
+
+        foreach ($this->plugin->list_folders() as $folder) {
+            if ($folder_types[$folder]) {
+                list($type, ) = explode('.', $folder_types[$folder]);
+            }
+            else {
+                $type = 'mail';
+            }
+
+            if (is_array($folder_groups[$type])) {
+                $folder_groups[$type][] = $folder;
+            }
+        }
+
+        // build block for every folder type
+        foreach ($folder_groups as $type => $group) {
+            if (empty($group)) {
+                continue;
+            }
+            $attrib['type'] = $type;
+            $html .= html::div('subscriptionblock',
+                html::tag('h3', $type, $this->plugin->gettext($type)) .
+                $this->folder_subscriptions_block($group, $attrib));
+        }
+
+        $this->rc->output->add_gui_object('subscriptionslist', $attrib['id']);
+
+        return html::div($attrib, $html);
+    }
+
+    public function folder_subscriptions_block($a_folders, $attrib)
+    {
+        $alarms = ($attrib['type'] == 'event' || $attrib['type'] == 'task');
+
+        $table = new html_table(array('cellspacing' => 0));
+        $table->add_header('subscription', $attrib['syncicon'] ? html::img(array('src' => $this->skin_path . $attrib['syncicon'], 'title' => $this->plugin->gettext('synchronize'))) : '');
+        if ($alarms) {
+            $table->add_header('alarm', $attrib['alarmicon'] ? html::img(array('src' => $this->skin_path . $attrib['alarmicon'], 'title' => $this->plugin->gettext('withalarms'))) : '');
+        }
+        $table->add_header('foldername', $this->plugin->gettext('folder'));
+
+        $checkbox_sync  = new html_checkbox(array('name' => 'subscribed[]', 'class' => 'subscription'));
+        $checkbox_alarm = new html_checkbox(array('name' => 'alarm[]', 'class' => 'alarm'));
+
+        $names = array();
+        foreach ($a_folders as $folder) {
+            $foldername = $origname = preg_replace('/^INBOX &raquo;\s+/', '', kolab_storage::object_name($folder));
+
+            // find folder prefix to truncate (the same code as in kolab_addressbook plugin)
+            for ($i = count($names)-1; $i >= 0; $i--) {
+                if (strpos($foldername, $names[$i].' &raquo; ') === 0) {
+                    $length = strlen($names[$i].' &raquo; ');
+                    $prefix = substr($foldername, 0, $length);
+                    $count  = count(explode(' &raquo; ', $prefix));
+                    $foldername = str_repeat('&nbsp;&nbsp;', $count-1) . '&raquo; ' . substr($foldername, $length);
+                    break;
+                }
+            }
+
+            $names[] = $origname;
+
+            $classes = array('mailbox');
+
+            if ($folder_class = rcmail_folder_classname($folder)) {
+                $foldername = rcube_label($folder_class);
+                $classes[] = $folder_class;
+            }
+
+            $folder_id = 'rcmf' . html_identifier($folder);
+            $padding = str_repeat('&nbsp;&nbsp;&nbsp;&nbsp;', $level);
+
+            $table->add_row(array('class' => (($level+1) * $idx++) % 2 == 0 ? 'even' : 'odd'));
+            $table->add('subscription', $checkbox_sync->show('', array('value' => $folder, 'id' => $folder_id)));
+
+            if ($alarms) {
+                $table->add('alarm', $checkbox_alarm->show('', array('value' => $folder, 'id' => $folder_id.'_alarm')));
+            }
+
+            $table->add(join(' ', $classes), html::label($folder_id, $padding . Q($foldername)));
+        }
+
+        return $table->show();
+    }
+}
