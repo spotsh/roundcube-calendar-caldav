c666c6a757ed2d79ce5edae0aedb9580f851f7a7
Aleksander Machniak <machniak@kolabsys.com>
2012-12-13 14:37:41 +0100

    Fix hook name: storage_connect -> imap_connect

--- plugins/kolab_auth/kolab_auth.php
+++ plugins/kolab_auth/kolab_auth.php
@@ -43,7 +43,7 @@ class kolab_auth extends rcube_plugin
 
         // Hooks related to "Login As" feature
         $this->add_hook('template_object_loginform', array($this, 'login_form'));
-        $this->add_hook('storage_connect', array($this, 'imap_connect'));
+        $this->add_hook('imap_connect', array($this, 'imap_connect'));
         $this->add_hook('managesieve_connect', array($this, 'imap_connect'));
         $this->add_hook('smtp_connect', array($this, 'smtp_connect'));
 
