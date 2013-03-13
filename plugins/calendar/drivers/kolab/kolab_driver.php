ccd9cc5bb88184299715644dae124bd000785e9c
Thomas Bruederli <bruederli@kolabsys.com>
2013-03-13 15:30:12 +0100

    Don't display local-change warning if calendar owner is organizer; Better wording for wanring text (#1693)

--- plugins/calendar/drivers/kolab/kolab_driver.php
+++ plugins/calendar/drivers/kolab/kolab_driver.php
@@ -128,6 +128,7 @@ class kolab_driver extends calendar_driver
         'class_name' => $cal->get_namespace(),
         'default'  => $cal->storage->default,
         'active'   => $cal->storage->is_active(),
+        'owner'    => $cal->get_owner(),
       );
     }
 
