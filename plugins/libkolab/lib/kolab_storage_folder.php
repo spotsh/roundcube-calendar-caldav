ee30f6fb8368ba8ee59fcdcd805d41eaef126f33
Aleksander Machniak <machniak@kolabsys.com>
2013-10-09 10:01:42 +0200

    Fix typo in last commit

--- plugins/libkolab/lib/kolab_storage_folder.php
+++ plugins/libkolab/lib/kolab_storage_folder.php
@@ -433,7 +433,7 @@ class kolab_storage_folder
         // string equals type query
         // FIXME: should not be called this way!
         if (is_string($query)) {
-            return $this->cache->has_type_col() && !empty($type) ? array(array('type','=',$query)) : array();
+            return $this->cache->has_type_col() && !empty($query) ? array(array('type','=',$query)) : array();
         }
 
         foreach ((array)$query as $i => $param) {
