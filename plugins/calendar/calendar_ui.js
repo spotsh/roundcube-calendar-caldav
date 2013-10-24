902fd08aaadfb93fd0eedb53fd8addc4f2418358
Daniel Morlock <morlock@videmo.de>
2013-10-24 14:25:48 +0200

    Introduced helper get_cal_driver(), further set driver attribute right before invoking HTTP requests to support all event actions.

--- plugins/calendar/calendar_ui.js
+++ plugins/calendar/calendar_ui.js
@@ -80,6 +80,15 @@ function rcube_calendar_ui(settings)
 
     /***  private methods  ***/
 
+    // Returns driver name for the given calendar id.
+    var get_cal_driver = function(cal_id)
+    {
+      var $input_driver = $("input[name=\"cal_" + cal_id + "\"]");
+      if($input_driver.length > 0) return $input_driver.val();
+
+      return null;
+    };
+
     // same as str.split(delimiter) but it ignores delimiters within quoted strings
     var explode_quoted_string = function(str, delimiter)
     {
@@ -700,7 +709,6 @@ function rcube_calendar_ui(settings)
         }
 
         data.calendar = calendars.val();
-        data.driver = $("input[name=\"cal_" + data.calendar + "\"]").val();
 
         if (event.id) {
           data.id = event.id;
@@ -1553,8 +1561,10 @@ function rcube_calendar_ui(settings)
     // post the given event data to server
     var update_event = function(action, data)
     {
+      var driver = get_cal_driver(data.calendar);
+
       me.saving_lock = rcmail.set_busy(true, 'calendar.savingdata');
-      rcmail.http_post('calendar/event', { action:action, e:data, driver:data.driver });
+      rcmail.http_post('calendar/event', { action:action, e:data, driver:driver });
       
       // render event temporarily into the calendar
       if ((data.start && data.end) || data.id) {
