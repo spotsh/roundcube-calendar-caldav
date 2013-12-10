b054d1492d5457e4446d14a3bf493b7037098df2
Daniel Morlock <morlock@videmo.de>
2013-12-10 19:23:35 +0100

    Added correct SQL for creating caldav tables.

--- plugins/calendar/drivers/caldav/SQL/mysql.initial.sql
+++ plugins/calendar/drivers/caldav/SQL/mysql.initial.sql
@@ -12,16 +12,12 @@
  *
  **/
 
- CREATE TABLE IF NOT EXISTS `caldav_props` (
-  `id` int(11) NOT NULL,
-  `type` enum('vcal','vevent','vtodo','') NOT NULL,
+CREATE TABLE IF NOT EXISTS `caldav_props` (
+  `obj_id` int(11) NOT NULL,
+  `obj_type` enum('vcal','vevent','vtodo','') NOT NULL,
   `url` varchar(255) NOT NULL,
   `tag` varchar(255) DEFAULT NULL,
   `user` varchar(255) DEFAULT NULL,
-  `pass` varchar(255) DEFAULT NULL
+  `pass` varchar(255) DEFAULT NULL,
+  UNIQUE KEY `obj_id` (`obj_id`,`obj_type`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
-ALTER TABLE `roundcube`.`caldav_props` ADD UNIQUE (
-  `obj_id` ,
-  `obj_type`
-);
