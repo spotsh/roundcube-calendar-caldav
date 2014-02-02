f505dfebaa7b7a39acea745ea572607b69344ef1
Daniel Morlock <daniel.morlock@gmail.com>
2014-02-02 16:06:22 +0100

    Add copyright.

--- plugins/calendar/drivers/caldav/SQL/mysql.initial.sql
+++ plugins/calendar/drivers/caldav/SQL/mysql.initial.sql
@@ -1,16 +1,24 @@
 /**
- * Roundcube Calendar
- *
- * Plugin to add a calendar to Roundcube.
+ * CalDAV Client
  *
  * @version @package_version@
- * @author Lazlo Westerhof
- * @author Thomas Bruederli
- * @url http://rc-calendar.lazlo.me
- * @licence GNU AGPL
- * @copyright (c) 2010 Lazlo Westerhof - Netherlands
+ * @author Daniel Morlock <daniel.morlock@awesome-it.de>
  *
- **/
+ * Copyright (C) 2013, Awesome IT GbR <info@awesome-it.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 CREATE TABLE IF NOT EXISTS `caldav_props` (
   `obj_id` int(11) NOT NULL,
